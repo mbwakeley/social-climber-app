ee46396978fa9c8960477b1c5efe817aec662136
mbwakeley <44876447+mbwakeley@users.noreply.github.com>
2020-08-13 15:25:46 -0700

    Saving point

--- src/component/Scream.js
+++ src/component/Scream.js
@@ -4,7 +4,9 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import Link from "react-router-dom/Link";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 const styles = {
   card: {
@@ -21,6 +23,7 @@ const styles = {
 
 export class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -33,7 +36,7 @@ export class Scream extends Component {
         commentCount,
       },
     } = this.props;
-    console.log(this.props);
+    console.log(createdAt);
     return (
       <Card className={classes.card}>
         <CardMedia
@@ -50,6 +53,9 @@ export class Scream extends Component {
             color="primary"
           >
             {userHandle}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             {body}
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,66 @@
 import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import AppIcon from "../images/images.png";
+import TextField from "@material-ui/core/TextField";
+
+const styles = {
+  form: {
+    textAlign: "center",
+  },
+  image: {
+    margin: "20px auto 20px auto",
+  },
+};
 
 export class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {},
+    };
+  }
+
+  handleSubmit = (e) => {
+    console.log("hi");
+  };
+  handleChanged = (e) => {};
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>login page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="climber" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default login;
+login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(login);
